33512927369228bd01604856410efae02035fd72
OwenOD <owen.barry.odonoghue@gmail.com>
2018-06-26 10:26:02 +1000

    added a trigger of the frount door for a sound and a meaagae

---
+++ The House/Assets/The House Assets/Scripts/LockedDoor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    public GameObject LockedDoorText;
+    public AudioSource LockedDoorSound;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKey (KeyCode.E))
+            {
+                LockedDoorText.SetActive(true);
+                Debug.Log("Pressing E");
+                LockedDoorSound.Play();
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            LockedDoorText.SetActive(false);
+        }
+    }
+}
